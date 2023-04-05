999f3f08f0be01a8f0482174abab41256078a796
amora92 <amocodes@yahoo.com>
2023-04-05 21:35:02 +0100

    events_index.js

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -1,7 +1,8 @@
 const Page = () => {
-    return (
+    return <div>
         <h1>About Us Page </ h1>
-    )
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+        </div>
 }
 
 export default Page;
