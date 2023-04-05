42675c27a75cf2fdbd6552172ce6ec4267644359
amora92 <amocodes@yahoo.com>
2023-04-05 21:49:40 +0100

    added_dynamic_page
    added_dynamic_page

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,9 +1,9 @@
 const Page = () => {
     return <div>
-    <h1>Events Page</h1>
-    <div>
+    <h1>Events Page index.js</h1>
+            <div>
 
-    <a href="">
+        <a href="">
           <img />
           <h2> Events in London</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
@@ -26,9 +26,8 @@ const Page = () => {
           <h2> Events in Vilnius</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
         </a>
-        
-    </div>
-    </div>
+            </div>
+        </div>
 }
 
 export default Page;
