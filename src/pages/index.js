79db3c264558a8834506a203970462b381477dbe
amora92 <amocodes@yahoo.com>
2023-04-06 19:30:37 +0100

    async_function_testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -79,7 +79,10 @@ export default function Home({ title }) {
   );
 }
 
-export function getServerSideProps() {
+export async function getServerSideProps() {
+  const data = await import('/data/data.json');
+  console.log(data);
+
   return {
     props: {
       title: "Hello everyone, I'm a prop",
